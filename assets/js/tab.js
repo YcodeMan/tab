aeb2e1a4e759bf34143951e4b2fba8d880674bbe
YcodeMan <1130578809@qq.com>
2019-03-26 17:06:33 +0800

    add tabInital

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -16,7 +16,7 @@
 	TabSwitch.prototype = {
 		inital: function () {
 			var self = this;
-			self.setDate();
+			self.setData();
 			self.tabInital();
 		},
 		/**
@@ -38,7 +38,7 @@
 			}
 			return defaultConfig;
 		},
-		setDate: function () {
+		setData: function () {
 			var tab = getComputedStyle(this.tab_cont[0]),
 				tab_w = parseInt(tab.width),
 				tab_h = parseInt(tab.height),
@@ -48,32 +48,103 @@
 			
 			switch (this.opts.changeMethod) {
 				case 'horizontal':
-					this.tabCont_wrap.style.width = tab_len * tab_w + 'px';
-					this.setCss(this.tab_cont, 'float: left')
+					setCss(this.tabCont_wrap, 'width', tab_len * tab_w + 'px')
+					setCss(this.tab_cont, 'float', 'left')
 					break;
 				case 'vertical':
-					this.tabCont_wrap.style.height = tab_len * tab_h + 'px';
+					setCss(this.tabCont_wrap, 'hiegth', tab_len * tab_h + 'px')
 					break;
 				case 'default':
 				case 'opacity':
 				default :
-					this.setCss(this.tab_cont, 'display: none');
+					setCss(this.tab_cont, 'display', 'none');
 				break;
 					
 			}
 		},
-		setCss: function (elem, value) {
-			for (var i = 0, len = elem.length; i < len; i++) {
-				elem[i].style = value;
+		tabInital: function () {
+			var index = this.index - 1;
+			
+			removeClass(this.tab_list, 'item-cur');
+			addClass(this.tab_list[index], 'item-cur');
+			
+			switch (this.opts.changeMethod) {
+				case 'horizontal':
+					var w = -index * parseInt(getComputedStyle(this.tab_cont[0]).width) + 'px';
+						setCss(this.tabCont_wrap, 'left', w);
+					break;
+				case 'vertical':
+					var h = -index * parseInt(getComputedStyle(this.tab_cont[0]).height) + 'px';
+						setCss(this.tabCont_wrap, 'top', h);
+					break;
+				case 'opacity':
+				case 'default':
+				default :
+					setCss(this.tab_cont[index], 'display', 'block');
+					break;
 			}
 		},
-		tabInital: function () {
+		changeTab: function (index) {
 			
 		}
 	}
 	window.tab = function (options) {
 		options = options || {};
 		new TabSwitch(options).inital();
+	}
+	/**
+	 * addClass 添加class
+	 * @param {Object} elem
+	 * @param {Object} cName
+	 */
+	function addClass(elem, cName) {
+		var i = 0,
+			len = elem.length;
+		if (len > 1) {
+			for (; i < len; i++) {
+				elem[i].className += " " + cName; 
+			}
+		} else {
+			elem.className += " " + cName;
+		}
+		
+	}
+	/**
+	 * removeClass 移出class
+	 * @param {Object} elem
+	 * @param {Object} cName
+	 */
+	function removeClass(elem, cName) {
+		var i = 0,
+			len = elem.length;
+		if (len > 1) {
+			for (; i < len; i++) {
+				elem[i].className = elem[i].className.replace(
+									new RegExp("(\\s|^)" + cName + "(\\s|$)" ), " "
+								)
+			}
+		} else {
+			elem.className =  elem.className.replace(
+									new RegExp("(\\s|^)" + cName + "(\\s|$)" ), " "
+								)
+		}
+	}
+	/**
+	 * setCss 设置css样式
+	 * @param {Object} elem
+	 * @param {Object} attr
+	 * @param {Object} value
+	 */
+	function setCss(elem, attr, value) {
+		var i = 0,
+			len = elem.length;
+		if (len > 1) {
+			for (; i < len; i++) {
+				elem[i].style[attr] = value;
+			}
+		} else {
+			elem.style[attr] = value;
+		}
 	}
 	
 })(window, document)
