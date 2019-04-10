cda013efcb837681c2964451ef54a9fdc3edd8dd
YcodeMan <1130578809@qq.com>
2019-04-10 19:43:43 +0800

    modify tab

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -308,7 +308,7 @@
 	 var EventUtil = {
 	 	addHandler: function (elem, type, handler, useCapture) {
 	 		if (elem.addEventListener) {
-	 			useCapture = typeof useCapture === 'boolean' ? useCapture : false;
+	 			var	useCapture = typeof useCapture === 'boolean' ? useCapture : false;
 	 			elem.addEventListener(type, handler, useCapture);
 	 		} else if (elem.attachEvent) {
 	 			elem.attachEvent('on' + type, handler);
